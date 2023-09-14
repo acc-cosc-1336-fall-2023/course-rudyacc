e67faaf88cea2be11caeb840cd00b8144a688562
rudyacc <143045796+rudyacc@users.noreply.github.com>
2023-09-14 00:21:07 +0000

    if elif menu example

--- src/examples/c_decisions/main.py
+++ src/examples/c_decisions/main.py
@@ -1,18 +1,3 @@
 import decisions
 
-result = decisions.is_overtime(40)
-
-if(result == False):
-    print('30 is overtime')
-
-
-if(result):
-    print('Is overtime')
-else: 
-    print("Not overtime")
-    
-grade = int(input ("Enter a numerical grade: "))
-
-letter_grade = decisions.get_letter_grade
-
-print ("Grade is: " + letter_grade)
+decisions.run_menu()
