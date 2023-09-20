d82e590bb671c563f62ebc132ced1b3e63b5caea
rudyacc <143045796+rudyacc@users.noreply.github.com>
2023-09-20 23:05:28 +0000

    us sum of squares in main

--- src/examples/d_repetition/main.py
+++ src/examples/d_repetition/main.py
@@ -3,4 +3,7 @@ import repetition
 #repetition.display_numbers(2)
 #repetition.prompt_user()
 #repetition.for_intro_loop()
-repetition.for_intro_loop_strings()
+#repetition.for_intro_loop_strings()
+
+result = repetition.for_sum_of_squares(5)
+print("sum of squares is: " + str(result))
