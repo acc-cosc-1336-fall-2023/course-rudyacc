2a5a36100a639d038ed84a21d7af03c287594c09
rudyacc <143045796+rudyacc@users.noreply.github.com>
2023-09-24 18:12:07 +0000

    homework 3 repetition tabs

--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,24 @@
-#
+def get_factorial(num):
+    product = 1
+    if num < 0:
+        return "Invalid, you can't factorial negative numbers"
+    else:
+        for n in range(1,num+1):
+            product *= n
+        return product
+    
+
+def sum_odd_numbers(num):
+    sum = 0
+
+    while num > 0:
+        if ((num % 2) == 0):
+            num = num - 1
+        sum += num
+        num = num - 2
+
+    return sum
+
+#like i want to get this but its been bugging me that i cant
+def sum_odd_numbers():
+    print 
--- tests/homework/d_repetition/tests_repetition.py
+++ tests/homework/d_repetition/tests_repetition.py
@@ -1,1 +1,24 @@
-#
+import unittest
+
+from src.homework.d_repetition.repetition import get_factorial
+from src.homework.d_repetition.repetition import sum_odd_numbers
+
+class Test_Config(unittest.TestCase):
+
+    def test_get_factorial(self):
+        self.assertEqual(get_factorial(4), 24)
+        self.assertEqual(get_factorial(5), 120)
+        self.assertEqual(get_factorial(6), 720)
+        self.assertEqual(get_factorial(0), 1)
+        self.assertEqual(get_factorial(-1), "Invalid, you can't factorial negative numbers")
+    
+    def test_sun_odd_numbers(self):
+        self.assertEqual(sum_odd_numbers(7), 16)
+        self.assertEqual(sum_odd_numbers(9), 25)
+        self.assertEqual(sum_odd_numbers(10), 25)
+        
+#repetition is hard
+    
+    
+    
+    
