81058c4070617b482d2fbcbe9141b8f0542d0f97
rudyacc <143045796+rudyacc@users.noreply.github.com>
2023-10-01 02:32:12 +0000

    homework 4 epoch seconds attempt 1

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,9 @@
-#
+import value_return
+
+hour = value_return.get_hour(3800)
+minutes = value_return.get_minutes(3800)
+seconds = value_return.get_seconds(3800)
+
+time = (f"{hour:02d}:{minutes:02d}:{seconds:02d}")
+
+print(time)
