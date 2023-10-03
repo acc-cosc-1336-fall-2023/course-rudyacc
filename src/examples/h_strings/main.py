b4f6c9c29f44ca5e01d32080f3ab9e2d7df957e7
rudyacc <143045796+rudyacc@users.noreply.github.com>
2023-10-03 00:08:47 +0000

    string replacement "Python"

--- src/examples/h_strings/main.py
+++ src/examples/h_strings/main.py
@@ -1,7 +1,10 @@
-letter = 'A'
-print(letter)
+import strings
 
+#strings.loop_string_w_while("Python")
+#strings.loop_string_w_for("Python")
+#strings.loop_string_w_special_for("Python")
 lang = "Python"
+
+print(lang[0])
+lang = lang.replace('y', 'Y').replace('o', 'O')
 print(lang)
-print(len(lang))
-print(lang[2])
