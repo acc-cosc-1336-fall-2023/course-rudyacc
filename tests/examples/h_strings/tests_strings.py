ad2e44fe1a62077f833727ee11116943e73e8d0e
rudyacc <143045796+rudyacc@users.noreply.github.com>
2023-10-05 00:00:04 +0000

    built-in string functions

--- tests/examples/h_strings/tests_strings.py
+++ tests/examples/h_strings/tests_strings.py
@@ -37,5 +37,46 @@ class Test_Config(unittest.TestCase):
         self.assertEqual(str1 < str2, True)
         self.assertEqual(str2 < str1, False)
         self.assertEqual("C++" == "C++ ", False)
+        
+    def test_is_string_digit(self):
+        str1 = "1200"
+        self.assertEqual(str1.isdigit(), True)
+        self.assertEqual("ab".isdigit(), False)
+        
+    def test_string_is_alpha(self):
+        str1 = "abcdefg"
+        self.assertEqual(str1.isalpha(), True)
+        self.assertEqual("$%1".isalpha(), False)
+
+    def test_string_is_upper(self):
+        str1 = "XYZ"
+        self.assertEqual(str1.isupper(), True)
+        
+    def test_string_to_lower(self):
+        str1 = "aBcDeF"
+        self.assertEqual(str1.lower(), "abcdef")
+        
+    def test_string_to_upper(self):
+        str1 = "aBcDeF"
+        self.assertEqual(str1.upper(), "ABCDEF")
+    
+    def test_strip_char_from_string(self):
+        str1 = "eeabcdefegefee"
+        self.assertEqual(str1.strip("e"), "abcdefegef")
+    
+    def test_lstrip_char_from_strings(self):
+        str1 = "aaaaxyz"
+        self.assertEqual(str1.lstrip("a"), "xyz")
+    
+    def test_rstrip_char_from_strings(self):
+        str1 = "abcyyy"
+        self.assertEqual(str1.rstrip("y"), "abc")
+        
+    def test_replace_from_string(self):
+        str1 = "eeabcdefegefee"
+        self.assertEqual(str1.replace("e", ""), "abcdfgf")
+        
+    
+        
 
   
