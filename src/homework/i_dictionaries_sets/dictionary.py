301670048fecb9f0d7272b32c33e8f77c8282eec
rudyacc <143045796+rudyacc@users.noreply.github.com>
2023-10-21 17:21:59 +0000

    Homework 6 P Distance Matrix

--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -1,1 +1,21 @@
-#
+def get_p_distance(list1, list2):
+    if len(list1) != len(list2):
+        raise ValueError("Sequences must have the same length")
+    
+    num_differences = sum(1 for i in range(len(list1)) if list1[i] != list2[i])
+    return num_differences / len(list1)
+
+def get_p_distance_matrix(sequences):
+    n = len(sequences)
+    p_distance_matrix = [[0.0] * n for _ in range(n)]
+    
+    for i in range(n):
+        for j in range(n):
+            p_distance_matrix[i][j] = get_p_distance(sequences[i], sequences[j])
+    return p_distance_matrix
+
+
+    
+    
+
+    
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,31 @@
-#
+import unittest
+from src.homework.i_dictionaries_sets.dictionary import get_p_distance
+from src.homework.i_dictionaries_sets.dictionary import get_p_distance_matrix
+
+class Test_Config(unittest.TestCase):
+
+    def test_get_p_distance(self):
+        list1 = ['T','T','T','C','C','A','T','T','T','A']
+        list2 = ['G','A','T','T','C','A','T','T','T','C']
+        result = get_p_distance(list1, list2)
+        self.assertEqual(result, 0.4)
+        
+    def test_get_p_distance_matrix(self):
+        list1 = list1 = [
+                ['T','T','T','C','C','A','T','T','T','A'],
+                ['G','A','T','T','C','A','T','T','T','C'],
+                ['T','T','T','C','C','A','T','T','T','T'],
+                ['G','T','T','C','C','A','T','T','T','A']
+        ]
+        result = get_p_distance_matrix(list1)
+        
+        self.assertEqual([
+            [0.0, 0.4, 0.1, 0.1],
+            [0.4, 0.0, 0.4, 0.3],
+            [0.1, 0.4, 0.0, 0.2],
+            [0.1, 0.3, 0.2, 0.0]
+        ], result)
+    
+    
+
+
