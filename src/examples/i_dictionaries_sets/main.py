ee5bee7a3408323e62b22927c92eab7b31f36d01
rudyacc <143045796+rudyacc@users.noreply.github.com>
2023-10-23 23:51:46 +0000

    find key w in, for key value in items

--- src/examples/i_dictionaries_sets/main.py
+++ src/examples/i_dictionaries_sets/main.py
@@ -1,18 +1,14 @@
 #main program
 phonebook = {'Chris': '555-1111', 'Katie': '555-2222', 'Joanne': '555-3333'}
-#print(phonebook)
 
-#print(phonebook['Chris'])
-#print(phonebook['Katie'])
 
-for key in phonebook:
-    print(phonebook[key])
+exists = dictionaries.is_key_in_dictionary('Chris', phonebook)
+
+print(exists)
+
+for key, value in phonebook.items():
+    print(key, value)
     
-print("values only")
-for value in phonebook.values():
-    print(value)
-    
-print("dictionary item")
-for item in phonebook.items():
-    print(item)
+for name, number in phonebook.items():
+    print(name, number)
     
--- tests/examples/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/examples/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -6,4 +6,10 @@ class Test_Config(unittest.TestCase):
 
     def test_configuration(self):
         self.assertEqual(True, test_config())
+        
+    def test_is_key_in_dictionary(self):
+        phonebook = {'Chris': '555-1111', 'Katie' : '555-2222', 'Joanne': '555-3333'}
+        
+        self.assertEqual(is_key_in_dictionary)
+        self.assertEqual(is_key_in_dictionary)
 
