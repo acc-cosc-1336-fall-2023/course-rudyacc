c643e04fbe3ecd6cc39926f67975baebc5feb820
rudyacc <143045796+rudyacc@users.noreply.github.com>
2023-10-23 23:36:45 +0000

    intro to dictionary

--- src/examples/i_dictionaries_sets/main.py
+++ src/examples/i_dictionaries_sets/main.py
@@ -1,1 +1,18 @@
 #main program
+phonebook = {'Chris': '555-1111', 'Katie': '555-2222', 'Joanne': '555-3333'}
+#print(phonebook)
+
+#print(phonebook['Chris'])
+#print(phonebook['Katie'])
+
+for key in phonebook:
+    print(phonebook[key])
+    
+print("values only")
+for value in phonebook.values():
+    print(value)
+    
+print("dictionary item")
+for item in phonebook.items():
+    print(item)
+    
