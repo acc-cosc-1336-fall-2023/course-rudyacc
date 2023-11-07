a09f995b276f4bf5884718200d72bb9fb3aadf15
rudyacc <143045796+rudyacc@users.noreply.github.com>
2023-11-07 00:12:52 +0000

    city read list database

--- src/examples/f_files_exception/files.py
+++ src/examples/f_files_exception/files.py
@@ -1,1 +1,23 @@
-#
+from fileinput import filename
+
+
+def write_city_list_file():
+    cities = ['New York', 'Boston', 'Atlanta', 'Dallas']
+    
+    out_file = open(filename, 'w')
+    for city in cities:
+        out_file.write(city + '\n')
+        
+    out_file.close()
+    
+def read_city_list_file(file_name):
+    in_file = open(file_name, 'r')
+    cities = []
+    
+    for line in in_file:
+        cities.append(line.rstrip('\n'))
+        
+    print(cities)
+    
+
+        
--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -1,1 +1,4 @@
-#main program
+import files
+
+#files.write_city_list_file('cities.txt')
+files.read_city_list_file('cities.txt')
