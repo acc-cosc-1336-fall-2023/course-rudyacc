1bedf7ec9009a53441e1ca337f007ea0154e4f93
rudyacc <143045796+rudyacc@users.noreply.github.com>
2023-11-13 14:18:13 +0000

    random stuff

--- src/examples/j_classes/main.py
+++ src/examples/j_classes/main.py
@@ -1,20 +1,8 @@
-import bank_account
+import bank_account, atm, menu
 account = bank_account.BankAccount(50)
+my_atm = atm.ATM(account)
 
-
-print(account.get_balance())
-
-amount = int(input("Enter deposit amount: "))
-
-account.deposit(amount)
-
-print(account.get_balance())
-
-amount = int(input("Enter amount: "))
-
-account.withdraw(amount)
-
-print(account.get_balance())
+menu.run_menu(my_atm)
 
 
 
